dc754caae80123bcacc2986c98e7ed9f914074cb
iaguilar <iaguilar@epcon.com.mx>
2014-10-27 23:48:52 -0600

    Isidro

---
+++ Alluminati/Controllers/RegistroController.cs
@@ -0,0 +1,43 @@
+﻿using Alluminati.Helper;
+using Alluminati.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Alluminati.Controllers
+{
+    public class RegistroController : Controller
+    {
+        private AlluminatiEntities db = new AlluminatiEntities();
+
+        // GET: Registro
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult RegistroUsuario(tblUsuarios usuarioAlluminati)
+        {
+            // Proceso de Registro
+            usuarioAlluminati.Contrasena = Encripcion.SHA256Encrypt(usuarioAlluminati.Contrasena);
+            if (db.tblUsuarios.FirstOrDefault(u => u.Usuario == usuarioAlluminati.Usuario && u.Contrasena == usuarioAlluminati.Contrasena && u.Estatus == 1) != null)
+            {
+                usuarioAlluminati.Estatus = 1;
+                db.tblUsuarios.Add(usuarioAlluminati);
+                db.SaveChanges();
+                return Json(new { result = true, url = Url.Action("Index", "Login") });
+            }
+            else
+            {
+                return Json(new { result = false });
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Alluminati/Scripts/Registro/Registro.js
@@ -0,0 +1,109 @@
+﻿/// Autor: Isidro Aguilar
+/// Fecha: 21/10/2014
+/// Descripción: Funciones JavaScript del Login de Usuarios Alluminati
+
+$(document).ready(function () {
+    // Establecer default para bootbox.
+    bootbox.setDefaults({
+        title: 'Notifiación',
+        size: 'small',
+    });
+
+    // Validaciones.
+    $('#formRegistro').bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            Usuario: {
+                validators: {
+                    notEmpty: {
+                        message: ' '
+                    }
+                }
+            },
+            Contrasena: {
+                validators: {
+                    notEmpty: {
+                        message: ' '
+                    }
+                }
+            },
+            Nombre: {
+                validators: {
+                    notEmpty: {
+                        message: ' '
+                    }
+                }
+             },
+            ApellidoP: {
+                 validators: {
+                     notEmpty: {
+                         message: ' '
+                     }
+                 }
+             },
+            CorreoElectronico: {
+                 validators: {
+                     notEmpty: {
+                         message: ' '
+                     }
+                 }
+             },
+            Direccion: {
+                 validators: {
+                     notEmpty: {
+                         message: ' '
+                     }
+                 }
+            },
+            Telefono: {
+                validators: {
+                    notEmpty: {
+                        message: ' '
+                    }
+                }
+            },
+        }
+    }).on('success.form.bv', function (e) {
+        e.preventDefault();
+    });
+});
+
+//Login
+$("#formRegistro").submit(function () {
+    debugger;
+    $(this).data('bootstrapValidator').validate();
+    if ($(this).data('bootstrapValidator').isValid()) {
+        var contrasena = $("#Contrasena").val();
+        var validaContrasena = $("#ValidaContrasena").val();
+        if (contrasena === validaContrasena) {
+            $.ajax({
+                url: $("#Registrar").attr("data-url"),
+                type: 'POST',
+                data: $(this).serialize(),
+                success: function (data) {
+                    if (data.result === true) {
+                        window.location.replace(data.url);
+                    }
+                    else {
+                        $("#Contrasena").val('');
+                        bootbox.alert("Error. Verifique sus datos.");
+                    }
+                },
+                error: function (data) {
+                    $("#Contrasena").val('');
+                    bootbox.alert("Ha ocurrido un error.");
+                }
+            });
+        } else {
+            error: function (data) {
+                $("#Contrasena").val('');
+                bootbox.alert("Verifique que su contraseña sea igual que en la confirmación.");
+            }    
+        }
+    }
+    return false;
+});
